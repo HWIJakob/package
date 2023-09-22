f974fd08dd50122fa84adb903b28d3f811e7f3a6
SMILE CODE <134200282+HWIJakob@users.noreply.github.com>
2023-09-22 17:44:55 +0700

    Create function.js

---
+++ function.js
@@ -0,0 +1,12 @@
+ const menuToggle = document.getElementById('menu-toggle');
+                const navMenu = document.getElementById('nav-menu');
+                const body = document.getElementsByTagName('body')[0];
+                menuToggle.addEventListener('click', () => {
+                    navMenu.classList.toggle('hidden');
+                });
+                body.addEventListener('click', (event) => {
+                    if (!event.target.closest('#nav-menu') && !event.target.closest('#menu-toggle')) {
+                        navMenu.classList.add('hidden');
+                    }
+                });
+
