187ced8a32f462de5639f85530d0bdc2f83662aa
SMILE CODE <134200282+HWIJakob@users.noreply.github.com>
2023-09-22 18:18:22 +0700

    Update function.js

--- function.js
+++ function.js
@@ -10,3 +10,35 @@
                     }
                 });
 
+const searchInput = document.querySelector('input[name="search"]');
+                    const clearSearchBtn = document.querySelector('.clear-search-btn');
+
+                    // Set tombol clear search untuk disembunyikan saat halaman dimuat
+                    if (searchInput.value === '') {
+                        clearSearchBtn.classList.add('hidden');
+                    } else {
+                        clearSearchBtn.classList.remove('hidden');
+                    }
+
+                    // Event listener untuk input pencarian
+                    searchInput.addEventListener('input', function(event) {
+                        if (event.target.value === '') {
+                            clearSearchBtn.classList.add('hidden');
+                        } else {
+                            clearSearchBtn.classList.remove('hidden');
+                        }
+                    });
+
+                    // Event listener untuk tombol clear search
+                    clearSearchBtn.addEventListener('click', function(event) {
+                        event.preventDefault();
+
+                        // Kosongkan input pencarian
+                        searchInput.value = '';
+
+                        // Set tombol clear search kembali disembunyikan
+                        clearSearchBtn.classList.add('hidden');
+
+                        // Submit form setelah membersihkan input pencarian
+                        this.closest('form').submit();
+                    });
